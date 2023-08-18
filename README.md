a85d61aeaf1e8359161b8b87c93124c702043d2d
Aditi Jain <jaditi930@gmail.com>
2023-08-19 00:13:15 +0530

    origin fixed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Imager-Free-Images-App
 # Backend https://imager-api.onrender.com/
+# Check out the website live at https://imager-76bv.onrender.com/
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app=express()
 const cors=require("cors")
 
 const corsOptions ={
-    origin:['http://localhost:3000','https://imager-api.onrender.com/'], 
+    origin:['http://localhost:3000','https://imager-api.onrender.com/','https://imager-76bv.onrender.com/'], 
     credentials:true,            
 
 }
